0d272956367424418d1e4f2927b55ad2591253bf
Darius Scerbavicius <darius.scerb@gmail.com>
2014-11-24 22:38:22 +0000

    Added gas station

---
+++ leetcode/gas-station.py
@@ -0,0 +1,31 @@
+class Solution:
+    # @param gas, a list of integers
+    # @param cost, a list of integers
+    # @return an integer
+    def canGoUntil(self, start, gas, cost):
+        i = start
+        current = 0
+        
+        while current >= 0:
+            current += gas[i]
+            current -= cost[i]
+            i += 1
+            i %= len(gas)
+            if (i == start): 
+                if current < 0: return -1 # we made a circle but couldn't get to the point
+                break
+        
+        return i
+
+    def canCompleteCircuit(self, gas, cost):
+        i = 0
+        while True:
+            next = self.canGoUntil(i, gas, cost)
+            if i == next: return i
+            elif next < i: return -1
+            else: i = next
+            
+        return -1
+
+s = Solution()
+print s.canCompleteCircuit([2,4],[3,4])
