89eac11e319aaa061d43c0f565e29d68bb9ddd79
Darius Scerbavicius <darius.scerb@gmail.com>
2014-12-06 17:02:08 +0000

    Changed the structure of word-break.py

--- leetcode/word-break.py
+++ leetcode/word-break.py
@@ -1,10 +1,9 @@
 class Solution:
   def wordBreak(self, s, dict):
     dict = set(dict)
-    D = [False] * (len(s)+1)
-    D[len(s)] = True
-    for i in xrange(len(s), -1, -1):
+    D = [False] * len(s)
+    for i in xrange(0, len(s)):
       for j in xrange(i+1, len(s)+1):
-        if s[i:j] in dict and D[j]:
-          D[i] = True
-    return D[0]
+        if s[i:j] in dict and (i == 0 or D[i-1]):
+          D[j-1] = True
+    return D[-1]
